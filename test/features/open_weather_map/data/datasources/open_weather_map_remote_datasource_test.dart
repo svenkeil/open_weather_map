f5bbd96a4de1b4d6653ab0eaf3e21e0b32b52379
Pedro Lemos <lemozxpedro@gmail.com>
2020-11-21 00:36:09 -0300

    test: should throw ServerException if status code is not 200

--- test/features/open_weather_map/data/datasources/open_weather_map_remote_datasource_test.dart
+++ test/features/open_weather_map/data/datasources/open_weather_map_remote_datasource_test.dart
@@ -1,5 +1,6 @@
 import 'package:mockito/mockito.dart';
 import 'package:open_weather_map/core/config/api_key.dart';
+import 'package:open_weather_map/core/error/exceptions.dart';
 import 'package:open_weather_map/core/http_client/http_client.dart';
 import 'package:open_weather_map/features/open_weather_map/data/datasources/open_weather_map_remote_datasource.dart';
 import 'package:open_weather_map/features/open_weather_map/data/models/weather_info_model.dart';
@@ -19,9 +20,14 @@ void main() {
   });
 
   group('getWeatherInfoForCity', () {
+    String tCity;
+
+    setUpAll(() {
+      tCity = 'Fortaleza';
+    });
+
     test('should call GET method from httpClient with correct URL', () async {
       // Arrange
-      final tCity = 'Fortaleza';
       final endpoint =
           'http://api.openweathermap.org/data/2.5/weather?q=$tCity&units=metric&appid=$kAPIKey';
 
@@ -42,7 +48,6 @@ void main() {
     test('should return WeatherInfoModel from response if status code is 200',
         () async {
       // Arrange
-      final tCity = 'Fortaleza';
       final tWeatherInfoModel = WeatherInfoModel(
           cityName: 'Fortaleza',
           country: 'BR',
@@ -62,6 +67,19 @@ void main() {
       // Assert
       verify(httpClient.get(any));
       expect(result, tWeatherInfoModel);
+    });
+
+    test('should throw ServerException if status code is not 200', () async {
+      // Arrange
+      when(httpClient.get(any)).thenAnswer(
+        (_) async => HttpResponse(data: null, statusCode: 500),
+      );
+
+      // Act
+      final call = remoteDatasource.getWeatherInfoForCity;
+
+      // Assert
+      expect(() => call(tCity), throwsA(TypeMatcher<ServerException>()));
     });
   });
 }
